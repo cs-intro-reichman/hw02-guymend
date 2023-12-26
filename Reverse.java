1523d5d901a7a3bdbfad28dbe092ec6c58ad5885
guymend <guy.mendelson@post.runi.ac.il>
2023-12-26 09:36:32 +0200

    Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -5,5 +5,14 @@
 public class Reverse {
 	public static void main (String[] args){
 		//// Put your code here
+		String word = "hello";
+		for(int i = 0; i < word.length(); i++){
+			if( (i + 1) == word.length() ){
+				System.out.println(word.charAt(word.length() - 1 - i));
+			}else{
+				System.out.print(word.charAt(word.length() - 1 - i));
+			}
+		}
+		System.out.println("The middle character is " + word.charAt(word.length()/2));
 	}
 }
