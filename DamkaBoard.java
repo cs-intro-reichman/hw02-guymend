895e8298454ffedd589104cc291025f0cf1ed84c
guymend <guy.mendelson@post.runi.ac.il>
2023-12-27 12:09:43 +0200

    DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -8,7 +8,7 @@ public class DamkaBoard {
         int j = 0;
         int i = 0;
         while ( i < n ){
-			if(i/2 != 0){
+			if(i%2 != 0){
 					System.out.print(" ");
 			}
             while ( j < n ){
