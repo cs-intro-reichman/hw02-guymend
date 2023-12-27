1ae967eb1c7206ca14923c66639aa03503917dc8
guymend <guy.mendelson@post.runi.ac.il>
2023-12-27 12:07:11 +0200

    DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,5 +4,24 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
+		int n = Integer.valueOf(args[0]);
+        int j = 0;
+        int i = 0;
+        while ( i < n ){
+			if(i/2 != 0){
+					System.out.print(" ");
+			}
+            while ( j < n ){
+                j++;
+                if(j == n){
+                    System.out.println(" * ");
+                }else{
+                    System.out.print(" * ");
+                }
+            }
+
+            i++;
+            j = 0;
+        }
 	}
 }
