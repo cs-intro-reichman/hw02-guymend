b92ab9f31c3c3365f38a87d9ad6dfdea5855d0a2
guymend <guy.mendelson@post.runi.ac.il>
2023-12-27 12:33:52 +0200

    DamkaBoard.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -6,5 +6,28 @@
 public class OneOfEach {
 	public static void main (String[] args) {
 		//// Put your code here
+		boolean noGirl = true;
+		boolean noBoy = true;
+		int amountOfChildren = 0;
+		while( noBoy || noGirl ){
+			amountOfChildren++;
+			if(Math.random() > 0.5){
+				if(!noBoy){
+					System.out.println("g");
+				}else{
+					System.out.print("g ");
+				}
+				noGirl = false;
+			}else{
+				if(!noGirl){
+					System.out.println("b");
+				}else{
+					System.out.print("b ");
+				}
+				noBoy = false;
+			}
+		}
+
+		System.out.println("You made it... and you now have " + amountOfChildren + " children");
 	}
 }
