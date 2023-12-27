6a5a9911cb798002c25273d82d52759cb1cf157d
guymend <guy.mendelson@post.runi.ac.il>
2023-12-27 11:49:00 +0200

    Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -4,5 +4,17 @@
 public class Perfect {
 	public static void main (String[] args) {
 		//// Put your code here
+		int number = Integer.valueOf(args[0]);
+		int perfectNumber = 0;
+		for (int i = 1; i < (number / 2) + 1; i++ ) {
+			if(number % i == 0){
+				perfectNumber = perfectNumber + i;
+			}
+		}
+		if(perfectNumber == number){
+			System.out.println(number + " is a perfect number");
+		}else{
+			System.out.println(number + " is not a perfect number");
+		}
 	}
 }
